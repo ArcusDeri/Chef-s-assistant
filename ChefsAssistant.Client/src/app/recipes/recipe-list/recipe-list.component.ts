dc8cd11c67cd43e711fdbf02207955a07872116e
Marcin Michniak <marcin.michniak@outlook.com>
2022-01-28 17:00:48 +0100

    Populate RecipeDetailComponent and RecipeList
    Signed-off-by: Marcin Michniak <marcin.michniak@outlook.com>

--- ChefsAssistant.Client/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ChefsAssistant.Client/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Tiramisu', 'A delicious italian cake.', 'https://www.maxpixel.net/static/photo/1x/Meal-Delicious-Cake-Pie-Candy-Sweet-Dessert-4618227.jpg'),
+    new Recipe('Italian Pizza', 'The real pizza from Italy', 'https://www.italian-feelings.com/wp-content/uploads/2019/10/PIAZZA-940x627.jpg')
+  ];
 
   constructor() { }
 
